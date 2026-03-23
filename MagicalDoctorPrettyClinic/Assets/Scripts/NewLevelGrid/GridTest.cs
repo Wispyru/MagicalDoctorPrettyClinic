4bd2cbdcb29fe373551612f907d32f01484a677f
seppvanderwal <34765@ma-web.nl>
2026-03-23 14:57:59 +0100

    quic push for sharing progress

---
+++ MagicalDoctorPrettyClinic/Assets/Scripts/NewLevelGrid/GridTest.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class GridTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Grid grid = new Grid(10, 10);
+    }
+    
+}
