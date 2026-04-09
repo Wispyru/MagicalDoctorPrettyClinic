58a11386e61590f8349b8a7d19fe52a52eda3a93
keesbrown <114744966+keesbrown@users.noreply.github.com>
2026-04-09 14:49:37 +0200

    updated the VolumeSettings Script

--- Assets/Scripts/VolumeSettings.cs
+++ Assets/Scripts/VolumeSettings.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -5,5 +6,43 @@ using UnityEngine.UI;
 public class VolumeSettings : MonoBehaviour
 {
     [SerializeField] private AudioMixer mixer;
-    [SerializeField] private Slider musicSlider;        
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider SFXSlider;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            LoadVolume(); 
+        }
+        else
+        {
+            SetMusicVolume();
+            SetSFXVolume();
+        }
+    }
+    
+    public void SetMusicVolume()
+    {
+        float volume = musicSlider.value;
+        mixer.SetFloat("music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+    }
+    
+    public void SetSFXVolume()
+    {
+        float volume = SFXSlider.value;
+        mixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+
+    private void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+
+        SetMusicVolume();
+        SetSFXVolume();
+
+    }
 }
