683a867c7542b7a81c03ddd9638fe6ef76081131
seppvanderwal <34765@ma-web.nl>
2026-05-07 11:33:59 +0200

    start yokai data

---
+++ Assets/Scripts/GameplayScripts/YokaiData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[System.Serializable]
+public class YokaiData
+{
+    public string yokaiName;
+    public MedicineType yokaiWeakness;
+    public int yokaiHealth;
+    public string yokaiDescription;
+}
