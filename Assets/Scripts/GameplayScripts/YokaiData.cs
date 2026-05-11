f4ba8440ec45c84139d1144e285dba8c3a225a5f
seppvanderwal <34765@ma-web.nl>
2026-05-11 09:34:03 +0200

    data for yokai

--- Assets/Scripts/GameplayScripts/YokaiData.cs
+++ Assets/Scripts/GameplayScripts/YokaiData.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 
-[System.Serializable]
-public class YokaiData
+[CreateAssetMenu(fileName = "New Yokai", menuName = "Gameplay/Yokai Data")]
+public class YokaiData : ScriptableObject
 {
     public string yokaiName;
     public MedicineType yokaiWeakness;
-    public int yokaiHealth;
+    public int yokaiHealth = 10;
+    
+    public Sprite yokaiSprite;
+    public GameObject yokaiPrefab;
+
     public string yokaiDescription;
+    
 }
