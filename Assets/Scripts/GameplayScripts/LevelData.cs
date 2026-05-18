061ead0e476a11af012337dc2cdde5bbde00e592
Wispy <elizavoets@gmail.com>
2026-05-18 14:24:03 +0200

    added the animations to level data

--- Assets/Scripts/GameplayScripts/LevelData.cs
+++ Assets/Scripts/GameplayScripts/LevelData.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Video;
 
 [CreateAssetMenu(fileName = "LevelData", menuName = "Level/DataObject")]
 public class LevelData : ScriptableObject
@@ -8,4 +9,12 @@ public class LevelData : ScriptableObject
     public float MaxTimeInSeconds;
     public int RequiredPoints;
     [TextArea] public string Description;
+
+
+    // assigned animations
+    public VideoClip StartingAnimation;
+    public VideoClip IdleAnimation;
+    public VideoClip AttackAnimation;
+    public VideoClip WinAnimation;
+    public VideoClip LoseAnimation;
 }
